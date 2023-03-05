3b203208b89b63b75bd4a084129135fab4fdd470
Tyler Whitfield <ctw2800@yahoo.com>
2023-03-05 17:44:13 -0600

    Further repaired first ORF, passing 11/13 now

--- find_orf.py
+++ find_orf.py
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'(^(AUG)[AUGC]+(UAA|UAG|UGA)$)'
+    orf_pattern_str = r'((AUG)([AUGC]{3})*(UAA|UAG|UGA))'
     ##########################################################################
     #(?:AUG([ACGU]{3})(?!UAA|UAG|UGA))
 
