8adb5ca2e3a5e5f5335992f30db4cacebad5597c
Tyler Whitfield <ctw2800@yahoo.com>
2023-03-05 14:27:53 -0600

    Really rough. Only 6/13 doctests working in find_orf. Didn't really understand what to do in Translate_orf.py but implemented what I could from the readme. Main function needs to be editied, as it was directly copied.

--- find_orf.py
+++ find_orf.py
@@ -57,8 +57,8 @@ def vet_nucleotide_sequence(sequence):
     # any valid RNA and DNA sequence strings, respectively (and only strings of
     # RNA and DNA bases).
     # Read the docstring above for additional clues.
-    rna_pattern_str = r'AUCG'
-    dna_pattern_str = r'ATCG'
+    rna_pattern_str = r'^[AUCGaucg]*$'
+    dna_pattern_str = r'^[ATCGatcg]*$'
     ##########################################################################
 
     rna_pattern = re.compile(rna_pattern_str)
@@ -119,7 +119,7 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'AUG'
+    codon_pattern_str = r'^(AUG)(?!UAA|UAG|UGA)[AUGCaugc]{3}$'
     ##########################################################################
 
     codon_pattern = re.compile(codon_pattern_str)
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'(?:AUG(?:[ACGU]{3})*?(?:UAA|UAG|UGA))'
     ##########################################################################
 
     # Create the regular expression object
